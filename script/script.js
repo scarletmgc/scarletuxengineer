f37ddddfbd2089e9409bb6c5a9b8da716b2db712
scarletmgc <scarletmgc@gmail.com>
2024-04-27 22:15:36 -0400

    Add certifications and couses

--- script/script.js
+++ script/script.js
@@ -8,10 +8,28 @@ window.addEventListener('DOMContentloader', ()=>{
 
 let textSkills = ['bootstrap','git','javascript','CSS3','HTML5','react','illustrator','photoshop','figma','lottie-files'];
 let textHobbies = ['do-karate','fitness','cook','surf-online'];
-let textAcademies = ['ITLA','freeCodeCamp','google','IBM-skillBuild','alura-latam'];
+let textAcademies = ['ITLA','freeCodeCamp','google','IBM-skillBuild','alura-latam','linkedin-learning'];
 let skillsContent = document.querySelector("#skillsContenedor");
 let hobbiesContent = document.querySelector("#hobbiesContenedor");
 let academiesContent = document.querySelector("#academiesContenedor");
+let estudios={
+    ITLA:'Multimedia Tecnology',
+    freeCodeCamp:'Responsive Web Design Certification',
+    google:'Foundations of User Experience (UX) Design Google',
+    IBM-skillBuild:{'Diseño Web Profesional El Curso Completo, Práctico y desde 0','Introduction to HTML and CSS','Web Development Basics'},
+    alura-latam:{'Curso Online Lógica de programación: sumérgete en la programación con JavaScript',
+        'Curso Online Lógica de programación: explorar funciones y listas',
+        'Curso Online HTML5 y CSS3 parte 1: Mi primera página web',
+        'Curso Online HTML5 y CSS3 parte 2: Posicionamiento, listas y navegación'
+        'CSS: Flexbox y layouts responsivos',
+        'JavaScript para web: crea páginas dinámicas',
+        'JavaScript: manipulando elementos en el DOM',
+        'Javascript: consumiendo y manejando datos de una API',
+        'JavaScript: creando solicitudes',
+        'JavaScript: validando formularios'
+        },
+        linkedin-learning:'Woman and Technology'
+}
 
 
 
@@ -49,18 +67,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (let kindex = 0; kindex < textAcademies.length; kindex++) {
         let academiesElements = document.createElement("div");
-        let icons = document.createElement('img');
-        let h6TextAcademies = document.createElement("h6");   
-        
-        icons.setAttribute('src',`features/images/${textAcademies[kindex]}.png`);
+        let tituloContenedor = document.createElement("div");
+        let tarjetaFlip=`<div class="card transition">
+  <h2 class="transition">Heading here<br><small>description line here</small></h2>
+  <div class="cta-container transition"><a href="#" class="cta">Button</a></div>
+  <div class="card_circle transition"></div>
+</div>
+`
 
+
+        let icons = document.createElement('img');
+        let h6TextAcademies = document.createElement("h6"); 
+        tituloContenedor.classList.add('titulos');  
+       
+        icons.setAttribute('src',`features/images/${textAcademies[kindex]}.png`);
+        
+
+
+        //tituloContenedor.appendChild(cardCertificados);
         academiesElements.classList.add('academies'); 
-        academiesContent.append(academiesElements);
+        academiesContent.append(academiesElements,tituloContenedor);
+        tituloContenedor.innerHTML=tarjetaFlip;
+        //academiesContent.append(tituloContenedor);
         academiesElements.append(icons);
         academiesElements.appendChild(h6TextAcademies).textContent = textAcademies[kindex];
+        //tituloContenedor.innerHTML='<div></div>';
+        console.log(tituloContenedor);
         console.log(textAcademies[kindex]);
     }
-
 
 
 
